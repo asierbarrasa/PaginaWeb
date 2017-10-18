7bd4aefa146c29af5c3226c07c00b3b1ddc9371a
Aitor Diaz de Otazu <aitor@MacBook-Pro-de-Aitor.local>
2017-10-18 12:35:53 +0200

    Lab 2B

---
+++ php/InsertarPregunta.php
@@ -0,0 +1,14 @@
+<?php
+$link = mysqli_connect("localhost", "id2921858_swg26","SWG26","id2921858_quiz");
+$sql = "INSERT INTO Pregunta(email, enunciado, respCorrecta, respInc1, respInc2"
+.", respInc3, complejidad, tema) VALUES ('$_POST[email]', '$_POST[preg]'".
+", '$_POST[respC]', '$_POST[respI1]', '$_POST[respI2]', '$_POST[respI3]'".
+", '$_POST[comp]', '$_POST[tema]')";
+if(!mysqli_query($link, $sql))
+{
+  die('Error: '.mysqli_error($link));
+}
+echo "1 record added";
+echo "<p> <a href = 'Verdatos.php'> Ver registros </a>";
+mysqli_close($link);
+ ?>
---
+++ php/Verdatos.php
@@ -0,0 +1,16 @@
+<?php
+$link = mysqli_connect("localhost", "id2921858_swg26","SWG26","id2921858_quiz");
+$datos = mysqli_query($link, "select * from Pregunta");
+echo '<table border=1> <tr> <th> Email </th> <th> Enunciado </th> <th> Respuesta Correcta</th>'
+.'<th> Respuesta Incorrecta 1</th><th> Respuesta Incorrecta 2</th><th> Respuesta Incorrecta 3</th>'
+.'<th> Complejidad </th><th> Tema </th></tr>';
+
+while($row = mysqli_fetch_array($datos)){
+  echo '<tr><td>'.$row['email'].'</td><td>'.$row['enunciado'].'</td><td>'.$row['respCorrecta'].'</td><td>'
+  .$row['respInc1'].'</td><td>'.$row['respInc2'].'</td><td>'.$row['respInc3'].'</td><td>'
+  .$row['complejidad'].'</td><td>'.$row['tema'].'</td></tr>';
+}
+echo '<table>';
+$datos->close();
+mysqli_close($link);
+ ?>

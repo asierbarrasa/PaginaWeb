a199953d2f3d3d5d78280ac73931ecaba215bd14
Aitor Diaz de Otazu <aitor@MacBook-Pro-de-Aitor.local>
2017-10-31 09:38:47 +0100

    .

--- php/layoutUser.php
+++ php/layoutUser.php
@@ -13,16 +13,16 @@
 <body>
   <div id='page-wrap'>
     <header class='main' id='h1'>
+      <?php echo $_GET["email"]?> <img id="imgUser" src= '<?php echo $_GET['img']?>'><br>
         <span class="right" ><a href="/logout">Logout</a></span>
-        <span class="rigth"> <label id="user" value = '<?php echo $_GET["email"]?>'> <img id="imgUser" src= '<?php echo $_GET['img']?>'>
-        >
+
       <h2>Quiz: el juego de las preguntas</h2>
     </header>
     <nav class='main' id='n1' role='navigation'>
-    <span><a href='layout.php?email=<?php echo $_GET['email']?>&img="<?php echo $_GET['img']?>"'>Inicio</a></spam>
-		<span><a href='pregunta.html?email=<?php echo $_GET['email']?>&img="<?php echo $_GET['img']?>"'>Preguntas</a></spam>
-		<span><a href ='php/Verdatos.php?email=<?php echo $_GET['email']?>&img="<?php echo $_GET['img']?>"'>Ver Preguntas</a></spam>
-		<span><a href='creditos.html?email=<?php echo $_GET['email']?>&img="<?php echo $_GET['img']?>"'>Creditos</a></spam>
+    <span><a href='layoutUser.php?email=<?php echo $_GET['email']?>&img=<?php echo $_GET['img']?>'>Inicio</a></span>
+		<span><a href='preguntaUser.php?email=<?php echo $_GET['email']?>&img=<?php echo $_GET['img']?>'>Preguntas</a></span>
+		<span><a href ='Verdatos.php?email=<?php echo $_GET['email']?>&img=<?php echo $_GET['img']?>'>Ver Preguntas</a></span>
+		<span><a href='creditosUser.php?email=<?php echo $_GET['email']?>&img=<?php echo $_GET['img']?>'>Creditos</a></span>
 	</nav>
     <section class="main" id="s1">
 

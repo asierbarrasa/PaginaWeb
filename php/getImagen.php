ce9416ee67d2c2f4a3a9c1f92327eea04e5da49d
Aitor Diaz de Otazu <aitor@MacBook-Pro-de-Aitor.local>
2017-10-31 11:47:52 +0100

    bugs fixed

--- php/getImagen.php
+++ php/getImagen.php
@@ -2,9 +2,10 @@
 $email = $_POST["email"];
 $link = mysqli_connect("localhost", "id2921858_swg26","SWG26","id2921858_quiz");
 
-$sql = "Select img FROM Users Where email = '".$email."'";
+$sql = "Select * FROM Users Where email = '".$email."'";
 $result = mysqli_query($link, $sql);
+$row = mysqli_fetch_assoc($result);
 
-echo $result;
+echo $row['img'];
 
  ?>

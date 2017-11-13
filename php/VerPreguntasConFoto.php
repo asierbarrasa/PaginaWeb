01d551f0cd51114a386a1cfcfb7e73940e44b9e8
Aitor Diaz de Otazu <aitor@MacBook-Pro-de-Aitor.local>
2017-11-13 18:35:33 +0100

    Cambiada disposicion tabla preguntas

--- php/VerPreguntasConFoto.php
+++ php/VerPreguntasConFoto.php
@@ -5,13 +5,13 @@ echo '<style> th {
     background-color: #4CAF50;
     color: white;
 }tr:nth-child(even) {background-color: #f2f2f2}</style><table border=1><tr><th> Enunciado </th> <th> Respuesta Correcta</th>'
-.'<th> Respuesta Incorrecta 1</th><th> Respuesta Incorrecta 2</th><th> Respuesta Incorrecta 3</th>'
+.'<th> Respuestas Incorrecta<s/th>'
 .'<th> Complejidad </th><th> Tema </th><th> Imagen </th></tr>';
 
 while($row = mysqli_fetch_array($datos)){
-  echo '<tr><td>'.$row['enunciado'].'</td><td>'.$row['respCorrecta'].'</td><td>'
-  .$row['respInc1'].'</td><td>'.$row['respInc2'].'</td><td>'.$row['respInc3'].'</td><td>'
-  .$row['complejidad'].'</td><td>'.$row['tema'].'</td><td><img src ="'.$row['imagen'].'" width="150px" heigth= "150px" ></td></tr>';
+  echo '<tr><td>'.$row['enunciado'].'</td><td>'.$row['respCorrecta'].'</td><td><ul><li>'
+  .$row['respInc1'].'</li><li>'.$row['respInc2'].'</li><li>'.$row['respInc3'].'</li></ul></td><td>'
+  .$row['complejidad'].'</td><td>'.$row['tema'].'</td><td><img src ="'.$row['imagen'].'" width="50px" heigth= "50px" ></td></tr>';
 
 
 }

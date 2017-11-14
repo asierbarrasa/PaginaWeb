5a96d0c9ce44ad86fae6fa2fd07034fb92b929c3
asierbarrasa <asierjarrillero@gmail.com>
2017-11-14 20:23:53 +0100

    ajax

---
+++ php/ContadorPreguntas.php
@@ -0,0 +1,12 @@
+<?php
+$link = mysqli_connect("localhost", "id2921858_swg26","SWG26","id2921858_quiz");
+$sql = "Select * from Pregunta";
+if(!$res=mysqli_query($link, $sql))
+{
+  die('Error: '.mysqli_error($link));
+}
+$numrow = mysqli_num_rows($res);
+echo $numrow;
+
+mysqli_close($link);
+ ?>
---
+++ php/ContadorPreguntasUsuario.php
@@ -0,0 +1,21 @@
+<?php
+$link = mysqli_connect("localhost", "id2921858_swg26","SWG26","id2921858_quiz");
+$sql = "Select * from Pregunta where email='".$_GET["email"]."'";
+$sql2 = "Select * from Pregunta";
+if(!$res=mysqli_query($link, $sql))
+{
+  die('Error: '.mysqli_error($link));
+}
+
+if(!$res2 = mysqli_query($link,$sql2))
+{
+    die('Error: '.mysqli_error($link));
+}
+$row1 = mysqli_num_rows($res);
+$row2 = mysqli_num_rows($res2);
+echo $row1;
+echo "/";
+echo $row2;
+
+mysqli_close($link);
+ ?>

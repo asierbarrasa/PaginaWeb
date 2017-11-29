897f58ce806c469cfe34858f1e2c53c80b9c7a42
Aitor Diaz de Otazu <aitor@MacBook-Pro-de-Aitor.local>
2017-11-29 13:52:45 +0100

    logOut

--- php/logOut.php
+++ php/logOut.php
@@ -7,6 +7,8 @@ $xml->numero[0]=$aux;
 $xml->asXML("../assets/xml/usuariosActivos.xml");
 header("Location: ../html/layout.html");
 
-
+//Recupera la sesión activa y la destruye.
+session_start();
+session_destroy();
 
 ?>

808f9e45748af28ad5abb77f6cf8702f08f2adb1
asierbarrasa <asierjarrillero@gmail.com>
2017-12-20 14:41:59 +0100

    corregir

--- php/Corregir.php
+++ php/Corregir.php
@@ -1,8 +1,9 @@
 <?php
 $respuesta = $_POST["respuesta"];
+$id=$_POST["id"];
 $link = mysqli_connect("localhost", "id2921858_swg26","SWG26","id2921858_quiz");
 
-$sql = "Select respCorrecta FROM Pregunta ";
+$sql = "Select respCorrecta FROM Pregunta WHERE id='".$id."'";
 $result = mysqli_query($link, $sql);
 $row = mysqli_fetch_assoc($result);
 if(mysqli_num_rows ($result)==1){

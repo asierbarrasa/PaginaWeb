9b42cf182142885111eb0457aa822c4f6f88768c
Aitor Diaz de Otazu <aitor@MacBook-Pro-de-Aitor.local>
2017-12-20 12:05:58 +0100

    .

--- php/EliminarPregunta.php
+++ php/EliminarPregunta.php
@@ -2,11 +2,7 @@
 $id = $_POST["id"];
 $link = mysqli_connect("localhost", "id2921858_swg26","SWG26","id2921858_quiz");
 
-<<<<<<< HEAD
 $sql = "DELETE from Pregunta Where id ='".$id."'";
-=======
-$sql = "DELETE * from Pregunta Where id ='id ";
->>>>>>> 705bedb21fc32f7a7d11d4801de710990eda7045
 
 
 if (mysqli_query($link, $sql)) {

a89c92b034eab1694d606fd6299714b0e7920898
asierbarrasa <asierjarrillero@gmail.com>
2017-12-20 11:12:35 +0100

    elimin

--- php/EliminarPregunta.php
+++ php/EliminarPregunta.php
@@ -1,6 +1,14 @@
 <?php
+$id = $_POST["id"];
 $link = mysqli_connect("localhost", "id2921858_swg26","******","id2921858_quiz");
 
-$sql = "DELETE * from Pregunta Where id =$_POST[id]";
+$sql = "DELETE * from Pregunta Where id ='id";
 
+
+if (mysqli_query($link, $sql)) {
+
+} else {
+    echo mysqli_error($link);
+}
+mysqli_close($link);
 ?>
